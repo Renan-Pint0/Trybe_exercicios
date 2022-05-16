96deeff2c98bfd66038e91d683d30072e7cd41b6
Renan Pinto <renanrnpinto@gmail.com>
2022-05-16 13:39:31 -0300

    Parte 2 realizada

--- fundamentos/bloco-4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ fundamentos/bloco-4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -1,8 +1,20 @@
 //Questão 01 - Calcular um fatorial//
 
-let factorial = 4;
+let factorial = 10;
 let result = 1;
 for (let index = 1; index <= factorial; index+= 1) {
     result = result*index;
 }
 console.log(result);
+
+// Questão 02 - Inverter uma palavra//
+
+let word = 'renan'
+let inverso = [];
+for (let index = word.length - 1; index >= 0; index-= 1) {
+   inverso.push(word[index]);
+} 
+var resultado = inverso.toString()
+console.log(resultado);   
+
+// Questão 3 -  //
