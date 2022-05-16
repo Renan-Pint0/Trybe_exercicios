98ce425c739ade9b693510826b6af10455309020
Renan Pinto <renanrnpinto@gmail.com>
2022-05-16 13:16:06 -0300

    parte 1

---
+++ fundamentos/bloco-4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -0,0 +1,8 @@
+//Questão 01 - Calcular um fatorial//
+
+let factorial = 4;
+let result = 1;
+for (let index = 1; index <= factorial; index+= 1) {
+    result = result*index;
+}
+console.log(result);
